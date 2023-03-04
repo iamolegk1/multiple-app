6a54e342c37c534da8c1effe66634657ea2625a0
iamolegk1 <oleg.kropivka5@gmail.com>
2023-03-04 23:22:29 +0200

    Add gp-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './i18n';
 
 import { store } from './redux/store';
